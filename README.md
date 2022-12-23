ffd9c734d78308c4f36961fe74cc14bfe45ced4f
svg83 <svgeneraloff@gmail.com>
2022-12-23 13:46:52 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# sci-fi_library
+ 
---
+++ render_website.py
@@ -0,0 +1,32 @@
+import json
+
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+
+
+
+with open("books_description.json", "r") as file:
+    books_description_json = file.read()
+
+books_description = json.loads(books_description_json)
+
+
+rendered_page = template.render(
+    books_description=books_description)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
+
+    
+
+##server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+##server.serve_forever()
