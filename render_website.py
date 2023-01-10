7c40b60609498b51c4e8b13a0a95be871033177b
svg83 <svgeneraloff@gmail.com>
2023-01-10 10:40:19 +0400

    добавлен парсер командной строки, исправлены ошибки

--- render_website.py
+++ render_website.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import logging
 import os
@@ -8,11 +9,25 @@ from math import ceil
 from more_itertools import chunked
 
 
-logger = logging.getLogger(__name__)
+def parse_cmd():
+    parser = argparse.ArgumentParser(description='''
+    Программа для формирования сайта библиотеки''')
+
+    parser.add_argument(
+        '-pb', dest='path_to_base',
+        type=str, default='books_description.json',
+        help='путь к файлу с описанием книг')
+
+    parser.add_argument(
+        '-bq', dest='books_quantity',
+        type=int, default=10,
+        help='количество книг на странице')
+
+    return parser.parse_args()
 
 
 def rebuild():
-    on_page_quantity = 10
+    args = parse_cmd()
     in_row_quantity = 2
 
     env = Environment(loader=FileSystemLoader('.'),
@@ -21,14 +36,12 @@ def rebuild():
     template = env.get_template('template.html')
     os.makedirs('pages', exist_ok=True)
 
-    with open("books_description.json", "r") as file:
+    with open(args.path_to_base) as file:
         books_description_json = file.read()
 
-    books_description = json.loads(books_description_json)
-
-    books_on_pages = list(chunked(books_description, on_page_quantity))
-
-    page_quantity = ceil(len(books_description)/len(books_on_pages))
+    books_descriptions = json.loads(books_description_json)
+    books_on_pages = list(chunked(books_descriptions, args.books_quantity))
+    page_quantity = ceil(len(books_descriptions)/args.books_quantity)
     page_numbers = list(range(1, page_quantity + 1))
 
     for current_page, books_on_page in enumerate(books_on_pages, 1):
@@ -39,17 +52,22 @@ def rebuild():
                                         last_page=page_quantity
                                         )
         page_path = os.path.join('pages', f'index{current_page}.html')
-        with open(page_path, 'w', encoding="utf8") as file:
+        with open(page_path, 'w', encoding='utf8') as file:
             file.write(rendered_page)
 
-    logger.info("Site rebuilt")
 
+def main():
 
-if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
 
     rebuild()
+    logger.info('Site rebuilt')
 
     server = Server()
     server.watch('template.html', rebuild)
     server.serve(root='.')
+
+
+if __name__ == "__main__":
+    main()
